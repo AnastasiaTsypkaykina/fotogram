ece189f2f080f88fc00fa29271ec6e4ad63fd990
Anastasia Tsypkaykina <tsypkaykina.anastasia@gmail.com>
2025-07-30 17:46:03 +0200

    some changes in style

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const images = [
-    {src: "../img/aalborg.jpg", alt: "aalborg"},
-    {src: "../img/aarhus.jpg", alt: "aarhus"},
-    {src: "../img/copenhagen1.jpg", alt: "copenhagen"},
-    {src: "../img/copenhagen2.jpg", alt: "copenhagen"},
-    {src: "../img/esbjerg.jpg", alt: "esbjerg"},
-    {src: "../img/kolding.jpg", alt: "kolding"},
-    {src: "../img/odense.jpg", alt: "odense"},
-    {src: "../img/randers.jpg", alt: "randers"},
-    {src: "../img/faaborg.jpg", alt: "faaborg"},  
-    {src: "../img/faborg.jpg", alt: "faborg"},   
-    {src: "../img/ribe.jpg", alt: "ribe"},   
-    {src: "../img/skagen.jpg", alt: "skagen"}       
+    {src: "./img/aalborg.jpg", alt: "aalborg"},
+    {src: "./img/aarhus.jpg", alt: "aarhus"},
+    {src: "./img/copenhagen1.jpg", alt: "copenhagen"},
+    {src: "./img/copenhagen2.jpg", alt: "copenhagen"},
+    {src: "./img/esbjerg.jpg", alt: "esbjerg"},
+    {src: "./img/kolding.jpg", alt: "kolding"},
+    {src: "./img/odense.jpg", alt: "odense"},
+    {src: "./img/randers.jpg", alt: "randers"},
+    {src: "./img/faaborg.jpg", alt: "faaborg"},  
+    {src: "./img/faborg.jpg", alt: "faborg"},   
+    {src: "./img/ribe.jpg", alt: "ribe"},   
+    {src: "./img/skagen.jpg", alt: "skagen"}       
 ];
 const container = document.getElementById("container");
 
