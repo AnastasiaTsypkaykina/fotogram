c20eb6a680fec43f81083230f1c37b4c3ba04cbf
Anastasia Tsypkaykina <tsypkaykina.anastasia@gmail.com>
2025-07-31 13:54:08 +0200

    dialog content added

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
 const images = [
-    "./img/aalborg.jpg",
-    "./img/aarhus.jpg",
-    "./img/copenhagen1.jpg",
-    "./img/copenhagen2.jpg",
-    "./img/esbjerg.jpg",
-    "./img/kolding.jpg",
-    "./img/odense.jpg",
-    "./img/randers.jpg",
-    "./img/faaborg.jpg",  
-    "./img/faborg.jpg",   
-    "./img/ribe.jpg",   
-    "./img/skagen.jpg"       
+  "./img/aalborg.jpg",
+  "./img/aarhus.jpg",
+  "./img/copenhagen1.jpg",
+  "./img/copenhagen2.jpg",
+  "./img/esbjerg.jpg",
+  "./img/kolding.jpg",
+  "./img/odense.jpg",
+  "./img/randers.jpg",
+  "./img/faaborg.jpg",
+  "./img/faborg.jpg",
+  "./img/ribe.jpg",
+  "./img/skagen.jpg",
 ];
-const container = document.getElementById("container");
 
 function renderImages() {
-    const container = document.getElementById("container");    
-    for (let i=0; i<images.length; i++) {
-        container.innerHTML+=getImagesHtml(i);
-    }    
+  const container = document.getElementById("container");
+  for (let i = 0; i < images.length; i++) {
+    container.innerHTML += getImagesHtml(i);
+  }
 }
 
-function getImagesHtml (i) {
-    return `<img class="photos" src="${images[i]}">`
+function getImagesHtml(i) {
+  return `<img onclick="createDialogContent(${i})" id="${i}" class="photos" src="${images[i]}">`;
 }
-
 
 function init() {
-    renderImages();
+  renderImages();
+}
+
+function toggleOverlay() {
+  let overlayRef = document.getElementById("overlay");
+  overlayRef.classList.toggle("d_none");
+}
+
+function createDialogContent(i) {
+  toggleOverlay();
+  let dialogRef = document.getElementById("overlay-dialog");
+  let imageID=document.getElementById("image-button")
+  dialogRef.innerHTML = getImageInDialogHtml(i);
+}
+
+function getImageInDialogHtml(i) {
+  return `
+    <div class="dialog-img-wrapper">
+        <img src="${images[i]}" class="dialog-img">
+    </div>   "`
 }
